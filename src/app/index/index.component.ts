839fea79f1ee482a49b71ab2294228119214124f
Malo Mouorn <malo.mouron@gmail.com>
2024-05-05 05:32:25 +0200

    Ajout des cards de capitaines

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {MatIcon} from "@angular/material/icon";
 import {MatRipple} from "@angular/material/core";
 import {Router} from "@angular/router";
 import {RoutePath} from "../navigation/route.path";
+import {PirateService} from "../../services/pirate.service";
+import {IPirateModel} from "../../models/pirate.model";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-index',
@@ -11,14 +14,26 @@ import {RoutePath} from "../navigation/route.path";
   imports: [
     NavbarComponent,
     MatIcon,
-    MatRipple
+    MatRipple,
+    MatProgressSpinner
   ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
 
-  constructor(private router : Router) {
+  public isLoading = true;
+
+  constructor(private router : Router, private pirateService: PirateService) {
+  }
+
+  public pirates : IPirateModel[] = [];
+
+  ngOnInit(): void {
+    this.pirateService.getPirates().subscribe(pirates => {
+      this.pirates = pirates;
+      this.isLoading = false;
+    })
   }
 
   goToDetails() {
