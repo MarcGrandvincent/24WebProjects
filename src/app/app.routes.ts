fdec841b25ca72cc53c6356b5822bd43eb9242df
Malo Mouorn <malo.mouron@gmail.com>
2024-05-05 03:02:14 +0200

    Add page detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import {RoutePath} from "./navigation/route.path";
 import {IndexComponent} from "./index/index.component";
+import {DetailComponent} from "./detail/detail.component";
 
 export const routes: Routes = [
   {
     path: RoutePath.index,
     component: IndexComponent,
+  },
+  {
+    path: RoutePath.detail,
+    component: DetailComponent,
   },
   { path: '', redirectTo: RoutePath.index, pathMatch: 'full' },
   { path: '**', redirectTo: RoutePath.index, pathMatch: 'full' }
