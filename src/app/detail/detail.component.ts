a5214c9ccffb76250e8ac7f027acbf68cc4c7ff1
Malo Mouorn <malo.mouron@gmail.com>
2024-05-05 03:29:19 +0200

    animation page detail

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  standalone: true,
+  styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements AfterViewInit {
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let elements = this.el.nativeElement.querySelectorAll('.sousBlocDetail');
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let positionFromTop = elements[i].getBoundingClientRect().top;
+      if(positionFromTop - window.innerHeight <= 0) {
+        element.classList.add('visible');
+      }
+    }
+  }
 
+  constructor(private el: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.onWindowScroll();
+  }
 }
